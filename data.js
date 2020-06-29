5693b5d787525cc17824c9472be1b0427e8a6f17
AnaRativa09 <anarativa09@gmail.com>
2020-06-29 18:22:16 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,13 @@
+// estas funciones son de ejemplo
+export const example = () => {
+ 
+ 
+
+
+  return 'example';
+};
+
+export const anotherExample = () => {
+ return './data/lol/lol.js'.filter()
+ 
+};
