1c2491c1f5a8c57a3a950bb8abc8f23455f84bb3
AnaRativa09 <anarativa09@gmail.com>
2020-07-12 20:53:46 -0500

    Updates

--- data.js
+++ data.js
@@ -1,13 +1,48 @@
-// estas funciones son de ejemplo
-export const filterData = (data, condition) => {
- 
- 
+/*------ Searchig -------*/
+export const searchingData = (lolRoles,champName) =>{
+  let lowerName = champName.toLowerCase();
+  let capitalUpperName = lowerName.charAt(0).toUpperCase() + lowerName.slice(1) 
+  let info=lolRoles.filter (function(champ) {
+    return champ.name.startsWith(capitalUpperName)
+  })
+  return info
+}
 
-
-  return 'filterData';
+/*------ Filter -------*/
+export const filterData = (lolRoles,rol) => {
+  let arrayRoles=lolRoles.filter(function(val){
+    return val.tags.includes(rol)
+   })
+   return arrayRoles 
 };
 
-export const anotherExample = () => {
- return './data/lol/lol.js'.filter()
- 
+/*------ Sorting -------*/
+export const sortingData = (lolRoles, order) => {
+  if (order == 'asc'){
+    let sortData = lolRoles.sort(function(a,z){
+      let nameA = a.name.toLowerCase()
+      let nameZ = z.name.toLowerCase()
+      if(nameA<nameZ){
+        return -1;
+      }if(nameA>nameZ){
+        return 1;
+      }
+      return 0;
+    });
+    return sortData
+  }
+  if (order == 'desc'){
+    let sortData = lolRoles.sort(function(a,z){
+      let nameA = a.name.toLowerCase()
+      let nameZ = z.name.toLowerCase()
+      if(nameA<nameZ){
+        return 1;
+      }if(nameA>nameZ){
+        return -1;
+      }
+      return 0;
+    });
+    return sortData
+  }
 };
+
--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 import data from './data/lol/lol.js';
-const lolData = Object.entries(data.data)
+import {filterData, searchingData, sortingData} from './data.js';
+let lolRoles = Object.values(data.data);
+console.log(lolRoles)
 
 let championsCards = document.querySelector('.card-champions');
 
-window.onload = () =>{
-const attribute = lolData.map(champions=> `
+const showingChamps = (data) =>{
+const attribute = data.map(champions=> `
   
-    <article style= "background-image:url('${champions[1].splash}')" class="card"> 
+    <article style= "background-image:url('${champions.splash}')" class="card"> 
       <div class="name-title-container">
-        <h2>${champions[1].name}</h2>
-        <p>${champions[1].title}</p>
+        <h2>${champions.name}</h2>
+        <p>${champions.title}</p>
       </div>
     </article>`
     
@@ -17,3 +19,61 @@ const attribute = lolData.map(champions=> `
 
  championsCards.innerHTML= attribute;
 };
+
+window.onload = function(){showingChamps(lolRoles)}
+
+/*------ Searchig names -------*/
+let searchChampionsMobile = document.querySelector('.search');
+searchChampionsMobile.addEventListener('keyup', searching);
+
+function searching (){
+  let champName = event.target.value;
+  let searchChamp = searchingData(lolRoles,champName);
+
+  if (champName ==" "){
+    showingChamps(lolRoles)
+  }else{
+    showingChamps(searchChamp)
+  }
+} 
+
+/*-------- Filter Select (mobile/tablet) -------- */
+let selectRoles = document.querySelector('.filter-select');
+selectRoles.addEventListener('change', checkRolesMobile);
+
+function checkRolesMobile(event){
+  let rol = event.target.value;
+  if (rol === 'Roles'){
+    showingChamps(lolRoles)
+  }else {
+    let result= filterData(lolRoles,rol)
+    showingChamps(result)
+  }
+}
+
+/*-------- Filter Checkbox (desktop) -------- */
+let checkboxRoles = document.querySelector('.roles');
+checkboxRoles.addEventListener('change', checkRoles);
+
+function checkRoles (event){
+  let rol = event.target.value;
+  let checkboxRol=document.getElementById(rol);
+  console.log(checkboxRol.checked)
+
+  if (checkboxRol.checked === true){
+    let result= filterData(lolRoles,rol)
+    showingChamps(result)
+  }else{
+    showingChamps(lolRoles)
+  }
+} 
+
+/*-------- Sorting -------- */
+let sortSelect = document.querySelector('.sort-select');
+sortSelect.addEventListener('change', sortingChamps);
+
+function sortingChamps (){
+  let order = event.target.value;
+  let sorting = sortingData(lolRoles,order)
+  showingChamps(sorting) 
+}
