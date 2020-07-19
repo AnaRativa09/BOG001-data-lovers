85c0de295b0268987be346e207f70d2fd83eea3d
Malejandraib <mariaibanez07@gmail.com>
2020-07-19 16:18:44 -0500

    computeStats, testing functions

--- src/data.js
+++ src/data.js
@@ -1,53 +1,49 @@
 /*------ Searchig -------*/
-export const searchingData = (lolRoles,champName) =>{
+export const searchData = (lolData,champName) =>{
   let lowerName = champName.toLowerCase();
   let capitalUpperName = lowerName.charAt(0).toUpperCase() + lowerName.slice(1) 
-  let info=lolRoles.filter (function(champ) {
+  let info=lolData.filter (function(champ) {
     return champ.name.startsWith(capitalUpperName)
   })
   return info
 }
 
 /*------ Filter -------*/
-export const filterData = (lolRoles,rol) => {
-  let arrayRoles=lolRoles.filter(function(val){
+export const filterData = (lolData,rol) => {
+  let arrayRoles=lolData.filter(function(val){
     return val.tags.includes(rol)
   })
   return arrayRoles 
 };
 
 /*------ Sorting -------*/
-export const sortingData = (lolRoles, order) => {
-  if (order == 'asc'){
-    let sortData = lolRoles.sort(function(a,z){
-      let nameA = a.name.toLowerCase()
-      let nameZ = z.name.toLowerCase()
-      if(nameA<nameZ){
-        return -1;
-      }if(nameA>nameZ){
-        return 1;
-      }
-      return 0;
-    });
-    return sortData
-  }
-  if (order == 'desc'){
-    let sortData = lolRoles.sort(function(a,z){
-      let nameA = a.name.toLowerCase()
-      let nameZ = z.name.toLowerCase()
-      if(nameA<nameZ){
-        return 1;
-      }if(nameA>nameZ){
-        return -1;
-      }
-      return 0;
-    });
-    return sortData
-  }
-};
-/*------ ComputeStats -------*/
-export const computeStats = () => {
+export const sortData = (lolData,order)=>{
 
- return 'lolRoles'
+  let dataSort = lolData.sort(function(a,z){
+    if(a.name > z.name){return 1;}
+    else {return -1;}
+    /* return 0; */
+  })
+  /* if(order === 'asc'){dataSort} */
+  if(order === 'desc'){dataSort.reverse()}
+  
+  return dataSort;
+}
+
+/*------ ComputeStats -------*/
+export const computeStats = (lolData) => {
+  let infoAttribute =lolData.map(function(attr){
+    let information = attr.info
+    let sum = information.attack + information.magic + information.defense + information.difficulty
+    let prom = sum/4
+    return Object.defineProperty(attr, 'prom',{value:prom, enumerable:true})
+  });
+  let order = infoAttribute.sort(function(a,b){
+    let prev = a.prom
+    let next = b.prom
+    return next-prev
+  });
+  let topFive = order.slice(0,5)
+  return topFive
 }
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import data from './data/lol/lol.js';
-import {/* filterData, */ searchingData, sortingData, filterData} from './data.js';
-import lol from './data/lol/lol.js';
-let lolRoles = Object.values(data.data);
-console.log(lolRoles)
-let activeData = [];
+import {searchData, sortData, filterData, computeStats} from './data.js';
+let lolData = Object.values(data.data);
 let rolTop = [];
+
 let championsCards = document.querySelector('.card-champions');
+let selectRoles = document.querySelector('.roles-select');
+let searchChampions = document.querySelector('.search');
+let sortSelect = document.querySelector('.sort-select');
 
 const showingChamps = (data) =>{
 const attribute = data.map(champions=> `
@@ -22,81 +23,91 @@ const attribute = data.map(champions=> `
 championsCards.innerHTML= attribute;
 };
 
-window.onload = function(){showingChamps(lolRoles)}
+window.onload = function(){showingChamps(lolData)}
+
 
 /*------ Searchig names -------*/
-let searchChampionsMobile = document.querySelector('.search');
-searchChampionsMobile.addEventListener('keyup', searching);
 
+searchChampions.addEventListener('keyup', searching);
 function searching (){
   let champName = event.target.value;
-  let searchChamp = searchingData(lolRoles,champName);
+  let searchingChamp = searchData(lolData,champName);
+ /*  let sortingData = sortData (searchingChamp, sortSelect.value);
+  let filteringData = filterData(sortingData, selectRoles.value); */
+  
+  showingChamps(searchingChamp)
 
   if (champName ==" "){
-    showingChamps(lolRoles)
-  }else{
-    showingChamps(searchChamp)
-    /* championsCards.style.justify-content:'flex-start'; */
+    showingChamps(lolData)
+  }else{    
+    
+    showingChamps(searchingChamp)
   }
 }
 
 /*-------- Filter Select (mobile/tablet/desktop) -------- */
-let selectRoles = document.querySelector('.roles-select');
-selectRoles.addEventListener('change', checkRolesMobile);
-
-function checkRolesMobile(event){
+selectRoles.addEventListener('change', checkRoles);
+function checkRoles(event){
   let rol = event.target.value;
+  let result= filterData(lolData,rol)
+  /* let sortingData = sortData (result, sortSelect.value);
+  let searchingChamp = searchData(sortingData, searchChampions.value); */
+
   if (rol === 'Roles'){
-    showingChamps(lolRoles)
-  }else {
-    let result= filterData(lolRoles,rol)
+    showingChamps(lolData)
+  }else {    
     showingChamps(result)
   }
 }
 
-
 /*-------- Sorting -------- */
-let sortSelect = document.querySelector('.sort-select');
-sortSelect.addEventListener('change', sortingChamps);
 
+sortSelect.addEventListener('change', sortingChamps);
 function sortingChamps (){
   let order = event.target.value;
-  let sorting = sortingData(lolRoles,order)
+ /*  let searchingChamp = searchData(lolData, searchChampions.value)
+  let filteringData = filterData(searchingChamp, selectRoles.value) */
+  let sorting = sortData(lolData, order)
   showingChamps(sorting)
 }
 
+/*-------- ComputeStats -------- */
 
 function stats (){
-  
-  let rails = event.target.value
-  if (rails === 'rail 1'){
-    array1 = filterData(lolRoles,'fighter');
-    array2 = filterData(lolRoles,'tank');
-    rolTop = array1.concat(array2)
+  let lane = 'Top'
+
+  if (lane ==='Lanes'){
+    rolTop = lolData
+    computeStats(rolTop)
+    showingChamps(computeStats(rolTop))
+  }
+
+  if (lane ==='Top'){
+    let firstRol = filterData(lolData,'Fighter');
+    let secondRol = filterData(lolData,'Tank');
+    rolTop = firstRol.concat(secondRol)
+    computeStats(rolTop)
+    showingChamps(computeStats(rolTop))
+  }
+  if (lane === 'Jungle'){
+    let firstRol = filterData(lolData,'Assassin');
+    let secondRol = filterData(lolData,'Tank');
+    rolTop = firstRol.concat(secondRol)
+    computeStats(rolTop)
+    showingChamps(computeStats(rolTop)) 
+  }
+  if (lane === 'Mid'){
+    let firstRol = filterData(lolData,'Assassin');
+    let secondRol = filterData(lolData,'Mage');
+    rolTop = firstRol.concat(secondRol)
+    computeStats(rolTop)
+    showingChamps(computeStats(rolTop))
+  }
+  if (lane === 'Bot'){
+    let firstRol = filterData(lolData,'Marksman');
+    let secondRol = filterData(lolData,'Support');
+    rolTop = firstRol.concat(secondRol)
+    computeStats(rolTop)
+    showingChamps(computeStats(rolTop))
   }
 }
-
-    /* let sumatoria = rolTop.reduce(function(atack, info.attack){
-      return
-    })
-  }
-}const array1 = [1, 2, 3, 4];
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
-console.log(accumulator)
-
-// 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer));
-// expected output: 10
-
-// 5 + 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer, 5));
-// expected output: 15
-
-
-var sumatoriaObjeto = arreglo.reduce(function(acumulador, siguienteValor){
-  return {
-    edad: acumulador.edad + siguienteValor.edad
-  };
-}, {edad: 0}); //Si no hay nada, regresamos un objeto con edad = 0. No hay necesidad de devolver el nombre, pues no es necesario
-
-var promedioEdad = sumatoriaObjeto.edad / arreglo.length; */
